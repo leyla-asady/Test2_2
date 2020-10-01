616a9de5fcf1ec20ab27361087d924c29fc2e750
leyla <asadyleyla2020@gmail.com>
2020-10-01 08:28:57 +0330

    Initial Commit

---
+++ Test2_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2_2
+//
+//  Created by Ali Asadi on 10/1/20.
+//  Copyright © 2020 Crouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
